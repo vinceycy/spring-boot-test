643acf221102217585142dff180c3b174aa99981
KaiXiangWu <sdf040200@gmail.com>
2017-11-19 21:10:00 +0800

    add controller

---
+++ src/main/java/com/example/demo/SimpleController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+	@RequestMapping("/")
+	public String index() {
+	    return "Greetings from Spring Boot!";
+	}
+}
